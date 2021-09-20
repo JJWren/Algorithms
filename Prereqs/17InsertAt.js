57519d54f5db1555b6f3007fa85416f8b4173637
Joshua Wren <43586816+JJWren@users.noreply.github.com>
2021-09-20 16:16:19 -0500

    Unfinished file 17

---
+++ Prereqs/17InsertAt.js
@@ -0,0 +1,10 @@
+/**
+ * Returns back array with value inserted at specified position: ([ 0, 1, 2, 3], "dog", 1) => [ 0, "dog", 1, 2, 3]
+ * @param {Array} array
+ * @param {} value
+ * @param {Number} position
+ */
+function insertAt(array, value, position) {
+    let subArr1 = [];
+    return array;
+}
